9a46f7f4c33334df1cdaea770789fe49a2b70295
KingJ <136109656+KingJ789@users.noreply.github.com>
2023-06-09 18:05:52 +0100

    Add files via upload
    this is personal if u open this I'm gonna cry unless ur my teacher

---
+++ Class 29.js
@@ -0,0 +1,3 @@
+function MyFunction(){
+    document.getElementById("Jashvin").innerHTML ="Always listen to whatever u want"
+}
---
+++ PROJECT 29.js
@@ -0,0 +1,3 @@
+function MyFunction(){
+    document.getElementById("Jashvin").innerHTML ="Bhourshetty"
+}
